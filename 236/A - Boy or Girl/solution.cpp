3b332bbcbdb6b0778029afdabb25767b60102bb4
Muhammad Bilal <muhammad.bilal18251@gmail.com>
2026-06-27 23:29:00 +0500

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+    char c;
+    set<char> s;
+    while (cin >> c)
+        s.insert(c);
+    cout << (s.size() % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
+}
