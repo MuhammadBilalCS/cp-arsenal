7d70970e3e41c647e3fbb4d9953c882e5def6290
Muhammad Bilal <muhammad.bilal18251@gmail.com>
2026-06-27 23:29:23 +0500

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  int arr[5][5];
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      cin >> arr[i][j];
+      if (arr[i][j] == 1)
+      {
+        cout << abs(i-2) + abs(j-2);
+      }
+    }
+  }
+}
