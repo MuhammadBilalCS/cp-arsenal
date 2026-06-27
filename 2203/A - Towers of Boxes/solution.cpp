befc2da94b75be1de3afe0a89d1204a6b278a5a4
Muhammad Bilal <muhammad.bilal18251@gmail.com>
2026-06-27 23:30:09 +0500

    Add Towers of Boxes [A] from Codeforces

---
+++ 2203/A - Towers of Boxes/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n,m,d;
+	int t;
+	cin>> t;
+	while(t--)
+	{
+	  
+	    cin>> n  >> m >> d;
+	    int maxHeight = (d / m) + 1;
+ 
+        int towers = (n + maxHeight - 1) / maxHeight;  
+ 
+        cout << towers << endl;
+	    
+	}
+}
